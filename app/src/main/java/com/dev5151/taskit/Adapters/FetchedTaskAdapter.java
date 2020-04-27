4ac205b67d0873a7a5ccf16e1a67b36f35c006ae
dev5151 <iamdev5151@gmail.com>
2020-04-27 16:06:11 +0530

    Added Search Item Layout

--- app/src/main/java/com/dev5151/taskit/Adapters/FetchedTaskAdapter.java
+++ app/src/main/java/com/dev5151/taskit/Adapters/FetchedTaskAdapter.java
@@ -13,12 +13,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.dev5151.taskit.Activities.DashboardActivity;
 import com.dev5151.taskit.R;
 import com.dev5151.taskit.models.Tasks;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class FetchedTaskAdapter extends RecyclerView.Adapter<FetchedTaskAdapter.FetchedTaskViewHolder> {
     private Context context;
     private ArrayList<Tasks> taskList;
+    private LatLng employerLatLng;
+    private LatLng employeeLatLng;
 
 
     public FetchedTaskAdapter(Context context, ArrayList<Tasks> taskList) {
@@ -35,16 +43,36 @@ public class FetchedTaskAdapter extends RecyclerView.Adapter<FetchedTaskAdapter.
     @Override
     public void onBindViewHolder(@NonNull FetchedTaskAdapter.FetchedTaskViewHolder holder, int position) {
         Tasks task = taskList.get(position);
-        holder.title.setText(task.getTitle());
-        /*holder.amount.setText(task.getBasePrice());
-        holder.extra.setText(task.getAmount());
-*/
-       /* holder.itemView.setOnClickListener(new View.OnClickListener() {
+        final String employerUid = task.getUid();
+        String employeeUid = FirebaseAuth.getInstance().getUid();
+        FirebaseDatabase.getInstance().getReference().child("users").child(employerUid).child("latLng").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                DashboardActivity.getBottomNavBarControlInterface().goToTask(task.getFlag(),task.getCreatorId());
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                employerLatLng = dataSnapshot.getValue(LatLng.class);
             }
-        });*/
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        FirebaseDatabase.getInstance().getReference().child("users").child(employeeUid).child("LatLng").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                employeeLatLng = dataSnapshot.getValue(LatLng.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        Integer distance = calculateDistanceInKilometer(employeeLatLng.latitude,employerLatLng.longitude,employerLatLng.latitude,employerLatLng.longitude);
+
+        holder.title.setText(task.getTitle());
+
     }
 
     @Override
@@ -63,5 +91,19 @@ public class FetchedTaskAdapter extends RecyclerView.Adapter<FetchedTaskAdapter.
         }
     }
 
+    public int calculateDistanceInKilometer(double userLat, double userLng,
+                                            double venueLat, double venueLng) {
+
+        double latDistance = Math.toRadians(userLat - venueLat);
+        double lngDistance = Math.toRadians(userLng - venueLng);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (int) (Math.round(DashboardActivity.AVERAGE_RADIUS_OF_EARTH_KM * c));
+    }
 
 }
--- app/src/main/java/com/dev5151/taskit/Fragments/FetchTaskFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/FetchTaskFragment.java
@@ -65,18 +65,5 @@ public class FetchTaskFragment extends Fragment {
         });
     }
 
-    public int calculateDistanceInKilometer(double userLat, double userLng,
-                                            double venueLat, double venueLng) {
 
-        double latDistance = Math.toRadians(userLat - venueLat);
-        double lngDistance = Math.toRadians(userLng - venueLng);
-
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
-                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return (int) (Math.round(DashboardActivity.AVERAGE_RADIUS_OF_EARTH_KM * c));
-    }
 }
