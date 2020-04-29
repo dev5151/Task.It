2fc948a800f4efad81b75d6d1bd89b248681de74
dev5151 <iamdev5151@gmail.com>
2020-04-29 19:44:14 +0530

    Fixed Splash Screen Handling

--- app/src/main/java/com/dev5151/taskit/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/dev5151/taskit/Activities/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.dev5151.taskit.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,10 @@ import android.os.Bundle;
 
 import com.dev5151.taskit.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -23,13 +28,35 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 if (mAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(SplashScreenActivity.this, LocationActivity.class));
-                    finish();
+                    fetchUser();
                 } else {
                     startActivity(new Intent(SplashScreenActivity.this, AuthActivity.class));
                     finish();
                 }
+
+                fetchUser();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    private void fetchUser() {
+        FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.getUid()).child("location").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String location = dataSnapshot.getValue(String.class);
+                if (location != null) {
+                    startActivity(new Intent(SplashScreenActivity.this, DashboardActivity.class));
+                    finish();
+                } else {
+                    startActivity(new Intent(SplashScreenActivity.this, LocationActivity.class));
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
