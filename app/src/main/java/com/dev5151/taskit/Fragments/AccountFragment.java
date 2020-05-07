b8674bd3eea532a0fde8c459a040cf679777270c
dev5151 <iamdev5151@gmail.com>
2020-05-07 17:42:35 +0530

    user data fetching

--- app/src/main/java/com/dev5151/taskit/Fragments/AccountFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/AccountFragment.java
@@ -10,20 +10,35 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.dev5151.taskit.Activities.DashboardActivity;
 import com.dev5151.taskit.R;
+import com.dev5151.taskit.models.User;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountFragment extends Fragment {
-    TextView name, phone, faq, about, wallet, share, rate, logout;
+    TextView tvName, tvPhone, faq, about, wallet, share, rate, logout;
     ImageButton edit;
+    MaterialToolbar toolbar;
+    DatabaseReference userRef;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
         initView(view);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle("My Profile");
+        fetchUser();
+
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,8 +49,8 @@ public class AccountFragment extends Fragment {
     }
 
     private void initView(View view) {
-        name = view.findViewById(R.id.name);
-        phone = view.findViewById(R.id.phone);
+        tvName = view.findViewById(R.id.name);
+        tvPhone = view.findViewById(R.id.phone);
         faq = view.findViewById(R.id.faq);
         about = view.findViewById(R.id.about);
         share = view.findViewById(R.id.share);
@@ -43,5 +58,30 @@ public class AccountFragment extends Fragment {
         rate = view.findViewById(R.id.rate);
         logout = view.findViewById(R.id.logout);
         edit = view.findViewById(R.id.edit);
+        toolbar = view.findViewById(R.id.toolbar);
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getUid());
+
+    }
+
+    private void fetchUser() {
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                String name = user.getName();
+                String phoneNum = user.getPhone();
+                setValue(name, phoneNum);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setValue(String name, String phoneNum) {
+        tvName.setText(name);
+        tvPhone.setText(phoneNum);
     }
 }
