01b4a7780c71f15876b8ac2866f22d841079ad53
dev5151 <iamdev5151@gmail.com>
2020-05-07 16:53:38 +0530

    Init Views

--- app/src/main/java/com/dev5151/taskit/Fragments/AccountFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/AccountFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +15,26 @@ import androidx.fragment.app.Fragment;
 import com.dev5151.taskit.R;
 
 public class AccountFragment extends Fragment {
+    TextView name, phone, faq, about, wallet, share, rate, logout;
+    ImageButton edit;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
-
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        name = view.findViewById(R.id.name);
+        phone = view.findViewById(R.id.phone);
+        faq = view.findViewById(R.id.faq);
+        about = view.findViewById(R.id.about);
+        share = view.findViewById(R.id.share);
+        wallet = view.findViewById(R.id.wallet);
+        rate = view.findViewById(R.id.rate);
+        logout = view.findViewById(R.id.logout);
+        edit = view.findViewById(R.id.edit);
     }
 }
--- app/src/main/java/com/dev5151/taskit/Fragments/HomeFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/HomeFragment.java
@@ -7,11 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.dev5151.taskit.Activities.LocationActivity;
 import com.dev5151.taskit.Adapters.PostedTasksAdapter;
 import com.dev5151.taskit.R;
