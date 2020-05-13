4c9e7e0301b3f675ce4dacdfbca6b128fda1b8e3
dev5151 <iamdev5151@gmail.com>
2020-05-14 02:22:11 +0530

    Added Logic

--- app/src/main/java/com/dev5151/taskit/Fragments/TasksRequestFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/TasksRequestFragment.java
@@ -10,13 +10,46 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.dev5151.taskit.R;
+import com.dev5151.taskit.models.TaskRequestModel;
+import com.dev5151.taskit.models.Tasks;
+import com.dev5151.taskit.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class TasksRequestFragment extends Fragment {
+
+    private DatabaseReference userRef;
+    private String uid;
+    private List<TaskRequestModel> taskRequestList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request_tasks, container, false);
+        fetchTaskRequestList();
         return view;
+    }
+
+    private void fetchTaskRequestList() {
+        userRef.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                taskRequestList.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    User user = dataSnapshot1.getValue(User.class);
+                    taskRequestList.addAll(0, user.getTaskRequestList());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
