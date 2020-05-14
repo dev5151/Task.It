50735be51932fcda72afd956e0d623f2849e3dc2
dev5151 <iamdev5151@gmail.com>
2020-05-14 13:02:57 +0530

    Bug Fixes

--- app/src/main/java/com/dev5151/taskit/Fragments/TasksRequestFragment.java
+++ app/src/main/java/com/dev5151/taskit/Fragments/TasksRequestFragment.java
@@ -11,13 +11,15 @@ import androidx.fragment.app.Fragment;
 
 import com.dev5151.taskit.R;
 import com.dev5151.taskit.models.TaskRequestModel;
-import com.dev5151.taskit.models.Tasks;
 import com.dev5151.taskit.models.User;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TasksRequestFragment extends Fragment {
@@ -30,8 +32,15 @@ public class TasksRequestFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request_tasks, container, false);
+        initViews(view);
         fetchTaskRequestList();
         return view;
+    }
+
+    private void initViews(View view) {
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
+        uid = FirebaseAuth.getInstance().getUid();
+        taskRequestList = new ArrayList<>();
     }
 
     private void fetchTaskRequestList() {
